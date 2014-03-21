6fc05246e25d130b01f480a456ef3cfd3b9b5130
Rhine.Liu <lyh_0100@sohu.com>
2014-03-21 16:51:45 +0800

    1.1.5.001

--- CellRush/js/guide.js
+++ CellRush/js/guide.js
@@ -9,10 +9,12 @@
 		fullScreen:1,
 		z:9999,
 		add:[
-			[Define.Element.Image, {image:"guide.jpg", alignParent:Define.Const.Align_CC}],
-			[Define.Element.Image, {x:330, y:310, image:"hand.png", _count:0, schedule:[function() {
-				this._count = (this._count+1)%30;
-				this.x(330-80*this._count/30);
+			[Define.Element.Image, {name:"bg", image:"guide.jpg", alignParent:Define.Const.Align_CC}],
+			[Define.Element.Image, {x:300, y:310, image:"hand.png", _count:0, schedule:[function() {
+				this._count = (this._count+1)%50;
+				if (this._count > 30) return;
+				this.x(Guide.bg.x()+300-80*this._count/30);
+				this.y(Guide.bg.y()+310);
 			}, 3]}]
 		],
 		showGuide:function(callback) {
